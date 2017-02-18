41d5d8b78d165573bcff4d7bd496bb603b4c3069
Slabkiy <slabkiya@gmail.com>
2017-02-18 15:40:32 +0300

    0.2.5 rewrite main page, add page "about"

---
+++ assets/js/sctipt.js
@@ -0,0 +1,54 @@
+if (window.location.href.split('#')[1] === undefined) {
+    openSection('main');
+} else {
+    openSection(window.location.href.split('#')[1]);
+}
+var a = document.getElementsByClassName('nav')[0].getElementsByTagName('a');
+for (var i = 0; i < a.length; i++) {
+    a[i].addEventListener('click', function(e) {
+        var id = e.target.parentElement.href.split('#')[1];
+        openSection(id);
+    });
+}
+
+function openSection(id) {
+
+    section = document.getElementsByTagName('section');
+    for (var i = 0; i < section.length; i++) {
+        section[i].classList.remove('active');
+        if (section[i].getAttribute('class') === id) {
+            section[i].classList.add('active');
+        }
+    }
+
+}
+var moduleTitle = document.getElementsByClassName('moduleTitle');
+for (var k = 0; k < moduleTitle.length; k++) {
+    var p = moduleTitle[k].getAttribute('data').split('\\');
+    for (var i = 0; i < p.length; i++) {
+        var span = p[i].split('');
+        var pr = document.createElement('p');
+        for (var j = 0; j < span.length; j++) {
+            var w = document.createElement('span');
+            w.innerText = span[j];
+            pr.appendChild(w);
+        }
+        moduleTitle[k].appendChild(pr);
+
+    }
+}
+var moduleParagraphs = document.getElementsByClassName('moduleParagraphs');
+for (var k = 0; k < moduleParagraphs.length; k++) {
+    var p = moduleParagraphs[k].getAttribute('data').split('\\');
+    for (var i = 0; i < p.length; i++) {
+        var span = p[i].split(' ');
+        var pr = document.createElement('p');
+        for (var j = 0; j < span.length; j++) {
+            var w = document.createElement('span');
+            w.innerText = " " + span[j];
+            pr.appendChild(w);
+        }
+        moduleParagraphs[k].appendChild(pr);
+
+    }
+}
